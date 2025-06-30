585eed3906082c7586706ea2f0e92374b565e2aa
asrani <asrani.27@gmail.com>
2025-06-30 17:04:16 +0800

    f

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,7 +10,7 @@
     <li class="{{ (request()->is('admin/data/pegawai*')) ? 'active' : '' }}"><a href="/admin/data/pegawai"><i
           class="fa fa-users"></i> <span>Data Pegawai</span></a></li>
     <li class="{{ (request()->is('admin/data/jadwal*')) ? 'active' : '' }}"><a href="/admin/data/jadwal">
-    <i class="fa fa-calendar"></i> <span>Jadwal Kegiatan</span> </a></li>
+        <i class="fa fa-calendar"></i> <span>Jadwal Kegiatan</span> </a></li>
     <li class="{{ (request()->is('admin/data/monitoring*')) ? 'active' : '' }}"><a href="/admin/data/monitoring"><i
           class="fa fa-users"></i> <span>Monitoring</span></a></li>
     <li class="treeview">
@@ -44,9 +44,9 @@
   <!-- Sidebar Menu -->
   <ul class="sidebar-menu" data-widget="tree">
     <li class="header">MENU UTAMA</li>
-    <li class="{{ (request()->is('pegawai')) ? 'active' : '' }}"><a href="/admin"><i class="fa fa-home"></i>
+    <li class="{{ (request()->is('pegawai')) ? 'active' : '' }}"><a href="/pegawai"><i class="fa fa-home"></i>
         <span>Beranda</span></a></li>
-     <li class="{{ (request()->is('pegawai/data/jadwal*')) ? 'active' : '' }}"><a href="/pegawai/data/jadwal"><i
+    <li class="{{ (request()->is('pegawai/data/jadwal*')) ? 'active' : '' }}"><a href="/pegawai/data/jadwal"><i
           class="fa fa-calendar"></i> <span>Jadwal DOOR TO DOOR</span></a></li>
     <li class="{{ (request()->is('pegawai/data/spt*')) ? 'active' : '' }}"><a href="/pegawai/data/spt"><i
           class="fa fa-file"></i> <span>Arsip SuratTugas</span></a></li>
@@ -63,11 +63,11 @@
     <li class="header">MENU UTAMA</li>
     <li class="{{ (request()->is('pimpinan')) ? 'active' : '' }}"><a href="/pimpinan"><i class="fa fa-home"></i>
         <span>Beranda</span></a></li>
-    <li class="{{ (request()->is('pimpinan/data/jadwalkegiatan*')) ? 'active' : '' }}"><a href="/pimpinan/data/jadwalkegiatan"><i
-          class="fa fa-calendar"></i> <span>Jadwal Kegiatan</span></a></li>
-    <li class="{{ (request()->is('pimpinan/data/monitoring*')) ? 'active' : '' }}"><a href="/pimpinan/data/monitoring"><i
-          class="fa fa-file"></i> <span>Monitoring</span></a></li>
-     <li class="treeview">
+    <li class="{{ (request()->is('pimpinan/data/jadwalkegiatan*')) ? 'active' : '' }}"><a
+        href="/pimpinan/data/jadwalkegiatan"><i class="fa fa-calendar"></i> <span>Jadwal Kegiatan</span></a></li>
+    <li class="{{ (request()->is('pimpinan/data/monitoring*')) ? 'active' : '' }}"><a
+        href="/pimpinan/data/monitoring"><i class="fa fa-file"></i> <span>Monitoring</span></a></li>
+    <li class="treeview">
       <a href="#">
         <i class="fa fa-file"></i> <span>Laporan</span>
         <span class="pull-right-container">

cc61045a2c4d6ccf6f74adac77ac35c95d4a5c7b
asrani <asrani.27@gmail.com>
2025-07-25 16:50:11 +0800

    f

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jadwal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class HomeController extends Controller
 {
     public function admin()
     {
-        return view('admin.home');
+        $data = Jadwal::with('okb')->get();
+
+        $grouped = $data->groupBy('kecamatan')->map(function (Collection $items, $kecamatan) {
+            $terdata = $items->filter(fn($item) => $item->okb?->first()?->hasil !== null)->count();
+            $tidakTerdata = $items->count() - $terdata;
+            $total = max($terdata + $tidakTerdata, 1);
+
+            return [
+                'kecamatan' => $kecamatan,
+                'terdata' => $terdata,
+                'terdata_persen' => round(($terdata / $total) * 100),
+                'tidak_terdata' => $tidakTerdata,
+                'tidak_terdata_persen' => round(($tidakTerdata / $total) * 100),
+            ];
+        })->values();
+        return view('admin.home', compact('grouped'));
     }
 
     public function pegawai()
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,9 +5,48 @@
 @section('content')
 <div class="row text-center">
   <img src="/background/logo.png" width="10%">
-<H2> HI,{{strtoupper(Auth::user()->name)}},SELAMAT DATANG DI APLIKASI DOOR TO DOOR<br/></H2>
+  <H2> HI,{{strtoupper(Auth::user()->name)}},SELAMAT DATANG DI APLIKASI DOOR TO DOOR<br /></H2>
 </div>
+<div class="container">
+  <div class="row">
+    @foreach ($grouped as $index => $item)
+    <div class="col-md-6 mb-4">
+      <div id="chartContainer{{ $index }}" style="height: 300px; width: 100%;"></div>
+    </div>
+    @endforeach
+  </div>
+</div>
+
 @endsection
 @push('js')
+
+<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+<script>
+  window.onload = function() {
+    @foreach ($grouped as $index => $item)
+    var chart{{ $index }} = new CanvasJS.Chart("chartContainer{{ $index }}", {
+        theme: "light2",
+        animationEnabled: true,
+        title: {
+            text: "Data Kecamatan {{ $item['kecamatan'] }}"
+        },
+        data: [{
+            type: "pie",
+            startAngle: 25,
+            toolTipContent: "<b>{label}</b>: {y} data",
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabelFontSize: 16,
+            indexLabel: "{label} - {y} data",
+          dataPoints: [
+              { y: {{ $item['terdata'] }}, label: "Sudah Terdata {{ $item['terdata'] }} ({{ $item['terdata_persen'] }}%)" },
+              { y: {{ $item['tidak_terdata'] }}, label: "Belum Terdata {{ $item['tidak_terdata'] }} ({{ $item['tidak_terdata_persen'] }}%)" }
+          ]
+        }]
+    });
+    chart{{ $index }}.render();
+    @endforeach
+}
+</script>
 
 @endpush
